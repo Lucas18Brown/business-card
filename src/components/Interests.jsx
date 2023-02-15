31d02216188b47cc02a422a62073e34fbe07fc47
lucas brown <lucas20brown@hotmail.co.uk>
2023-02-15 11:44:35 +0000

    added interests page

---
+++ src/components/Interests.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Interests() {
+  return (
+    <div className="interests" >
+      <h2 id="interests--title" >Interests</h2>
+      <p id="interests--para" >
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  )
+}
+
+export default Interests
