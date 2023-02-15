31a16db31257520d74fa8cfba64d026a289a8683
lucas brown <lucas20brown@hotmail.co.uk>
2023-02-15 11:45:54 +0000

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer() {
+  return (
+    <footer >
+      <a className="footer--github" href='https://github.com/Lucas18Brown' target="_blank" rel='noopener noreferrer' >
+        <img id="footer--github_img" src="assets/githubicon.png" alt="github icon" />
+      </a>
+    </footer>
+  )
+}
+
+export default Footer;
